da563bd0fab61421da522ef8b2fd8f9ed5a2fd21
Keracles <keracles10@gmail.com>
2022-11-04 13:23:23 +0100

    patch

--- bookmakers/zebet.py
+++ bookmakers/zebet.py
@@ -129,7 +129,7 @@ def get_league_matches(pattern, name_league):
     matches = []
     links = MatchsLinksScrap(pattern)
     for link in links :
-        url_match = url_zebet + url_match
+        url_match = url_zebet + link
         try : 
             match = build_match(url_match, name_league)
         except requests.exceptions.ReadTimeout:
